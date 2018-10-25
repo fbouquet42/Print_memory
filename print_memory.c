344f1c58d07b2c34f0493f1f7a4c1a6a58fcbff7
Felix BOUQUET <fbouquet@e1r5p5.42.fr>
2018-10-25 16:45:57 +0200

    brackets

--- print_memory.c
+++ print_memory.c
@@ -6,14 +6,13 @@
 /*   By: fbouquet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 11:19:37 by fbouquet          #+#    #+#             */
-/*   Updated: 2016/11/29 10:45:34 by fbouquet         ###   ########.fr       */
+/*   Updated: 2018/10/25 16:45:42 by fbouquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_put_hexa(int nb)
-{
+static void	ft_put_hexa(int nb) {
 	char	*str;
 
 	str = "0123456789abcdef";
@@ -21,10 +20,8 @@ void	ft_put_hexa(int nb)
 	write(1, (str + nb % 16), 1);
 }
 
-void	print_space(size_t n)
-{
-	while (n != 16)
-	{
+void	print_space(size_t n) {
+	while (n != 16) {
 		if ((n % 2))
 			write(1, " ", 1);
 		write(1, "  ", 2);
@@ -32,13 +29,11 @@ void	print_space(size_t n)
 	}
 }
 
-size_t	print_hexa(const unsigned char *addr, size_t size, size_t i)
-{
+size_t	print_hexa(const unsigned char *addr, size_t size, size_t i) {
 	size_t	n;
 
 	n = 0;
-	while (n + i != size && n != 16)
-	{
+	while (n + i != size && n != 16) {
 		ft_put_hexa(*(addr + i + n));
 		if (n % 2)
 			write(1, " ", 1);
@@ -47,13 +42,11 @@ size_t	print_hexa(const unsigned char *addr, size_t size, size_t i)
 	return (n);
 }
 
-void	print_char(const unsigned char *addr, size_t size, size_t i)
-{
+void	print_char(const unsigned char *addr, size_t size, size_t i) {
 	size_t	n;
 
 	n = 0;
-	while (n != 16 && n + i != size)
-	{
+	while (n != 16 && n + i != size) {
 		if (*(addr + i + n) < 32 || *(addr + i + n) > 126)
 			write(1, ".", 1);
 		else
@@ -62,13 +55,11 @@ void	print_char(const unsigned char *addr, size_t size, size_t i)
 	}
 }
 
-void	print_memory(const void *addr, size_t size)
-{
+void	print_memory(const void *addr, size_t size) {
 	size_t	i;
 
 	i = 0;
-	while (i < size)
-	{
+	while (i < size) {
 		print_space(print_hexa((const unsigned char*)addr, size, i));
 		print_char((const unsigned char*)addr, size, i);
 		write(1, "\n", 1);
